a84482f71b63c9d36de77d2d5bea2faad41e0ca9
NeoSvet <neosvet333@gmail.com>
2019-05-14 16:53:08 +0300

    add list

--- app/src/main/java/ru/neosvet/neomap/MarkersActivity.java
+++ app/src/main/java/ru/neosvet/neomap/MarkersActivity.java
@@ -1,19 +1,22 @@
 package ru.neosvet.neomap;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-
-import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.model.LatLng;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MarkersActivity extends AppCompatActivity {
+    private ListView lvMarkers;
+    private ArrayAdapter<String> adMarkers;
+    private List<Double> mLat = new ArrayList<Double>();
+    private List<Double> mLng = new ArrayList<Double>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +24,9 @@ public class MarkersActivity extends AppCompatActivity {
         setContentView(R.layout.activity_markers);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        initList();
+        loadList();
 
 //        FloatingActionButton fab = findViewById(R.id.fabEdit);
 //        fab.setOnClickListener(new View.OnClickListener() {
@@ -30,6 +36,31 @@ public class MarkersActivity extends AppCompatActivity {
 //                        .setAction("Action", null).show();
 //            }
 //        });
+    }
+
+    private void initList() {
+        List<String> liMarkers = new ArrayList<String>();
+        adMarkers = new ArrayAdapter<String>(MarkersActivity.this, android.R.layout.simple_list_item_1, liMarkers);
+        lvMarkers = (ListView)findViewById(R.id.lvMarkers);
+        lvMarkers.setAdapter(adMarkers);
+    }
+
+    private void loadList() {
+        DataBase db = new DataBase(this);
+        SQLiteDatabase sq = db.getWritableDatabase();
+        Cursor cursor = sq.query(DataBase.TABLE, null, null, null, null, null, null);
+        if (cursor.moveToFirst()) {
+            int iName = cursor.getColumnIndex(DataBase.NAME);
+            int iLat = cursor.getColumnIndex(DataBase.LAT);
+            int iLng = cursor.getColumnIndex(DataBase.LNG);
+            do {
+                adMarkers.add(cursor.getString(iName));
+                mLat.add(cursor.getDouble(iLat));
+                mLng.add(cursor.getDouble(iLng));
+            } while (cursor.moveToNext());
+        }
+        db.close();
+        adMarkers.notifyDataSetChanged();
     }
 
     @Override
